4913d27fa168f9de6d2312daf923c5d9d73352a5
Vegard Larsen <vegard.h.larsen@bi.no>
2026-05-23 15:01:46 +0200

    Update keymaps to remap H and L for line navigation
    Remap H and L keys to navigate to the start and end of the line.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Remap H and L to start/end of line, matching shell vi mode
+-- Original H = top-of-screen, L = bottom-of-screen (you lose these)
+vim.keymap.set({ "n", "x", "o" }, "H", "^", { desc = "Go to first non-blank char" })
+vim.keymap.set({ "n", "x", "o" }, "L", "$", { desc = "Go to end of line" })
